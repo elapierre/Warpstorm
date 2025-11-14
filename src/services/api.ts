df69de428a9c90fbe5448211f6b85b1852b6fba4
LaPierre, Eric <Eric.LaPierre@holman.com>
2025-11-14 14:47:01 -0500

    Fix duplicate folders, merge files, correct casing

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError, AxiosRequestConfig } from "axios";
 import Constants from "expo-constants";
-import { getToken, isTokenExpired, clearToken } from "./Auth/TokenManager";
-import { ApiError } from "../Types/ApiError";
+import { getToken, isTokenExpired, clearToken } from "./Auth/tokenManager";
+import { ApiError } from "../Types/apiError";
 
 /** The base URL for the Holman Upfit API. */
 const baseURL : string = Constants.expoConfig?.extra?.apiBaseUrl as string; 
